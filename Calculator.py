e54274e0c83a89f2e8fec85cf63d5eccd5361fa8
abc <samratsaha23122007@gmail.com>
2021-09-01 21:34:58 +0530

    added more operations to this calculator added ** and avg function

--- Calculator.py
+++ Calculator.py
@@ -1,13 +1,28 @@
+from math import *
 first_number=float(input("Enter the first number: "))
+
 second_number=float(input("Enter the second number: "))
+
 Operator=input("Enter operator: ")
+
+
 if Operator=="+":
-    print(f'The result is {first_number+second_number}')
+    print(f'The result is {first_number+second_number})
+
 elif Operator=="-":
     print(f'The result is {first_number-second_number}')
+
 elif Operator=="*":
-    print(f'The rsult is {first_number*second_number}')
+    print(f'The result is {first_number*second_number}')
+
 elif Operator=="/":
     print(f'The result is {first_number/second_number}')
+
+elif Operator=="**":
+    print(f'The result is {first_number**second_number}')
+
+elif Operator=="avg":
+    print(f'The result is {(first_number+second_number)/2}')
+
 else:
     print("Wrong operator")
